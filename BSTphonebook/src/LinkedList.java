84fbc45ccd2ec85cebe03eac9dd6867bb6d1d712
hadeelalabood <146745483+hadeelalabood@users.noreply.github.com>
2023-11-10 01:09:16 +0300

    hadeel LinkedList.java

--- BSTphonebook/src/LinkedList.java
+++ BSTphonebook/src/LinkedList.java
@@ -1,4 +1,35 @@
+class Node<T> {
+	private T data;
+	private Node<T> next;
 
+	public Node() {
+		data = null;
+		next = null;
+	}
+
+	public Node(T value) {
+		data = value;
+		next = null;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public Node<T> getNext() {
+		return next;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+
+}
+///////////////
 public class LinkedList <T extends Comparable<T>> {
 
 	private Node<T> head;
